8da3c91b8b37e3f6c5f42d24891349fde291c414
Gatunye Lucky <gatunyelucky@gmail.com>
2020-04-17 22:18:22 +0300

    initial-commit
    This is the first version of this library. I have been inspired by one of my studnets #shanitah to develop this simple library it is simple and easy to understant yet powerfull.
    
    Since this is the initail write. It is meant for beginners who would like to have powerfull web applications with view components inside thier  javascript and also have the ablity to setup state in thiser applications.

---
+++ example/sai-light.js
@@ -0,0 +1,140 @@
+/**
+  |--------------------INTRO-----------------------
+  | LIGHT-JS TOOLKIT BY GATUNYE LUCKY 
+  | This simple UI toolkit is ment to help you with simple application state management and 
+  | rendering of views when this is a state update
+  |  WARNING ----
+  | Please do not change the store shape manually use the setState and getState to get content form the 
+  | store object if you access it direclty you may break the structure.
+  | Since this is a simple toolkit and this is the inital version 0.1.0 
+  | Please be aware of the changes you make 
+  |-------------------------------------------------
+ */
+
+/**
+ * STEP ONE
+ * Let us setup a simple store for our data.
+ * This store is going to be a contant object,
+ * Since objects store data by reference it will be great for us to use it
+ */
+
+const STORE = {
+  state: {},
+  prevState: {},
+};
+
+/**
+ * After creating the store
+ * Lets create our view store, this will be used to store the views and thire refrences
+ * A view can simply be a simple ui segment that you can put into a string and then render it to the
+ * The point and spects of the views will make our pages more flexiable and easy to change
+ * e.g
+ */
+const VIEW_STORE = {
+  views: [],
+  history: [],
+};
+
+/**
+ * @function view(component) // this function takes in function that return a string of the compoent
+ * @param string component
+ * View string the the view it self.
+ * @example
+ *  view(() => {
+ *     return `<div>some html element here</div>`
+ * }).point('#todo-wrapper')
+ */
+function view(component) {
+  var _ref; // variable to hold dom referencing element
+  var _view = {}; // view object
+  var _name = "unamed"; // inital view name
+
+  const assignView = () => {
+    const _view_store = { ...VIEW_STORE }; // making a copy of the view_store
+    // we add this condition to check if the view does not exist
+    _view_store
+    if (_view_store.views.indexOf(_view) === -1) {
+      _view_store.views = [..._view_store.views, _view]; // add the view to the view list
+      VIEW_STORE.views = _view_store.views; // update the view_store object
+      render();
+    }
+  };
+
+  const _render = () => {
+    _view = { name: _name, component: component, ref: _ref };
+    assignView();
+  };
+  
+  return {
+    point: (ref) => {
+      _ref = document.querySelector(ref);
+      return {
+        render: _render,
+        named: (name) => {
+          _name = name;
+          return {
+            render: _render,
+          };
+        },
+      };
+    },
+  };
+}
+
+/**
+ * STEP TWO
+ * Since we shall be updating the ui after saving data in the store.
+ * We shall need to create a simple render function which will handle rendering the
+ * changes to the ui
+ */
+function render() {
+  const _views = { ...VIEW_STORE.views }; // make a copy of the store
+  for (index in _views) {
+    var _view = _views[index];
+    _view.ref.setAttribute(
+      "sai-view-update-time",
+      index + "999" + _view.name + new Date().getTime()
+    ); // adding a simple view attribute
+    _view.ref.innerHTML = _view.component();
+  }
+}
+
+/**
+ * STEP FOUR
+ * We need to have our store clean and fine, this means its not a good idea to simple just access it on the
+ * fly, we are going to create two methods
+ * ====[METHOD ONE]=====
+ * @function getState()
+ * this method will be used to create a copy of the actual state, since our state is an {object}
+ * any change we do it will affect the actual object.
+ * When it comes to reading the application state
+ * ====[METHOD TWO]
+ * @function setState(),
+ * ====[METHOD THREE]====
+ * @function prevState(),
+ */
+
+/**
+ *  Get the current application state
+ */
+function getState() {
+  const _state = { ...STORE.state };
+  return _state;
+}
+
+/**
+ * make an update to the state in the store object
+ */
+function setState(state) {
+  const _prev_state = { ...STORE.state };
+  const _store = { ...STORE };
+  _store.prevState = _prev_state;
+  _store.state = state;
+  STORE.prevState = _store.prevState;
+  STORE.state = _store.state;
+  return render();
+}
+
+/**
+  |===========PLEASE DON'T CHANGE THE ABOVE CODE UNLESS YOU KNOW WHAT YOU ARE DOING=====
+ */
---
+++ example/todos.js
@@ -0,0 +1,133 @@
+/**
+ * STEP ONE -- Create the initail application state
+ * Please use these steps to achive the best in your application
+ */
+
+setState({
+  todos: [ ],
+});
+
+/**
+ * create a ui component that has the html code
+ * since we need to have automatic update to the application we shall use the getState insde our component
+ */
+const TodoComponent = () => {
+  // we shall use this functioni to get the state stored in the global store
+  const state = getState();
+  var list = ""; // avariable to hold the todos after the irritating through the array
+  for (var index in state.todos) {
+    var todo = state.todos[index];
+    list += `
+     <li class="todo-item">
+        <div class="todo ${todo.completed ? "completed" : ""}" >
+            ${todo.todo}
+        </div>
+        <div class="todo-actions">
+          <div>
+            <button type="button" onClick="deleteTodo(${index})" class="delete">
+                Delete
+            </button>
+            <button type="button" class="complete" onClick="markTodoComplete(${index})">
+                ${todo.completed ? "Mark Incomplete" : "Mark Compelete"}"
+            </button>
+          </div>
+        </div>
+      </li>
+    `;
+  }
+  return list;
+};
+
+
+
+/**
+ * USE CASE ONE 
+ * We can choose to pass in the name of the function such that it is called by refrence 
+ * @function view(TodoComponent).point("#todo-wrapper").named("odo-list-view").render();
+ * You can use the above example simply copy the function call 
+ * [.point()] method will take the [ id | classname | or any other attribute  ]
+ */
+
+
+/* USE CASE TWO
+ * call the view function to render the view initally
+ * at the call of this function the view will be stored inside
+ */
+view(function () {
+  // we shall use this functioni to get the state stored in the global store
+ const state = getState();
+ var list = ""; // avariable to hold the todos after the irritating through the array
+ for (var index in state.todos) {
+   var todo = state.todos[index];
+   list += `
+     <li class="todo-item">
+        <div class="todo ${todo.completed ? "completed" : ""}" >
+            ${todo.todo}
+        </div>
+        <div class="todo-actions">
+          <div>
+            <button type="button" onClick="deleteTodo(${index})" class="delete">
+                Delete
+            </button>
+            <button type="button" class="complete" onClick="markTodoComplete(${index})">
+                ${todo.completed ? "Mark Incomplete" : "Mark Compelete"}"
+            </button>
+          </div>
+        </div>
+      </li>
+    `;
+ }
+ if(list == '')
+  {
+    list = "<p>You dont have any todo </P>"
+  }
+  return list;
+}).point("#todo-items-wrapper").named("todo-list-view").render();
+
+/**
+ * Adding some action
+ * here we shall get the content fom the todo and update the state 
+ * Once we update the state the render method will be automatically called 
+ */
+const input = document.getElementById('add-todo-input')
+const add_todo_button = document.getElementById('add-todo-button');
+
+add_todo_button.addEventListener('click', event => {
+
+  // get the todo item from the input value
+  const todo = input.value;
+
+  //let us get the state 
+  var  state = getState();
+
+  // affter geting our application state we push a new item to the todos
+  state.todos = [...state.todos, {todo:  todo, completed: false}]
+
+  //affter pushing a new item we call setState(state) method in order to update the state  
+  setState(state);
+
+  input.value = ""
+
+})
+
+/**
+ * When we delete the todo
+ */
+function deleteTodo(index){
+  var  state = getState();
+  var newTodoList = [];
+  for (var _index in state.todos) {
+    if (_index == index) {
+      continue;
+    }
+    newTodoList.push(state.todos[_index]);
+  }
+  state.todos = newTodoList;
+  setState(state)
+}
+
+function markTodoComplete(index){
+  var state = getState();
+  state.todos[index].completed = !state.todos[index].completed;
+  setState(state);
+}
